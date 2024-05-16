e97d5e75221a6f3aa9e0b81e5822d95c78312f90
JongD <JongD@JONGDO>
2024-05-16 12:38:53 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ChallengeLabs1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the temperature in Fahrenheit: ");
+            double temp = Convert.ToDouble(Console.ReadLine());
+            if (temp <= 10)
+            {
+                Console.WriteLine("Freezing weather");
+            }
+            else if (temp >= 11 && temp <= 20)
+            {
+                Console.WriteLine("Very cold weather");
+            }
+            else if (temp >= 21  && temp <= 35)
+            {
+                Console.WriteLine("Cold weather");
+            }
+            else if (temp >= 36  && temp <= 50)
+            {
+                Console.WriteLine("Normal in Weather");
+            }
+            else if (temp >= 51  && temp <= 65)
+            {
+                Console.WriteLine("Its hot");
+            }
+            else if (temp >= 66  && temp <= 80) 
+            {
+                Console.WriteLine("Its very hot");
+            }
+
+
+
+
+        }
+        
+
+        
+
+
+               
+
+
+
+        
+        
+    }  
+
+}
